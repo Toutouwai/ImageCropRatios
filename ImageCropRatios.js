768bd4364083858b3723618c819c92e1261d61a9
Robin Sallis <Toutouwai@users.noreply.github.com>
2020-03-16 13:24:37 +1300

    v0.1.0

---
+++ ImageCropRatios.js
@@ -0,0 +1,43 @@
+(function($) {
+
+	// Get parameter from URL
+	function getUrlParameter(name) {
+		name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+		var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+		var results = regex.exec(location.search);
+		return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+	}
+
+	// Set crop aspect ratio
+	function setCropAspectRatio() {
+		var value = $('#icr_ratio').val();
+		if(value !== 'auto') {
+			// Remove prefix and get float value
+			value = parseFloat(value.substring(1));
+		}
+		$('#selected_image').cropper('setAspectRatio', value);
+	}
+
+	$(document).ready(function() {
+
+		// Get aspect ratios from PW config
+		var parent_config = window.parent.ProcessWire.config;
+		var image_field_name = getUrlParameter('field');
+		var ratios = parent_config['icr_ratios_' + image_field_name];
+
+		// Add select element
+		var $el = $('<label>Ratio:&nbsp;</label>');
+		var $select = $('<select id="icr_ratio"></select>');
+		$.each(ratios, function(index, value) {
+			$select.append('<option value="' + index + '">' + value + '</option>');
+		});
+		$el.append($select);
+		$('#crop_coordinates').append($el);
+
+		// Set crop aspect ratio when crop action selected or dropdown changes
+		$('#crop_action').click(setCropAspectRatio);
+		$(document).on('change', '#icr_ratio', setCropAspectRatio);
+
+	});
+
+}(jQuery));
---
+++ README.md
@@ -0,0 +1,27 @@
+# Image Crop Ratios
+
+A module for ProcessWire CMS/CMF. Allows preset aspect ratios to be defined per image field for the ProcessWire image crop tool.
+
+The module adds a select dropdown to the crop tool. Choose an aspect ratio and the crop area will be fixed to that ratio.
+
+## Screencast
+
+![screencast](https://user-images.githubusercontent.com/1538852/76713927-05943180-6789-11ea-96c5-1386de908a5f.gif)
+
+
+## Installation
+
+[Install](http://modules.processwire.com/install-uninstall/) the Image Crop Ratios module.
+
+## Configuration
+
+* Default aspect ratios for all image fields can be defined in the module config.
+* Aspect ratios for specific image fields can be defined on the Input tab of the field settings.
+* You can override the ratio settings in template context if needed.
+* Insert a hyphen as the first item in the ratio settings unless you want to force a ratio to be applied to the crop tool. The hyphen represents a blank option that allows a free crop area to be drawn.
+
+## Usage
+
+1. Click the "Crop" link on the details view of an image thumbnail.
+2. Click the "Crop" icon at the top of the editor window.
+3. Choose an option from the "Ratio" select dropdown.
